ba25e5473b6c10537c685c6f9227fb4f2adeb01b
Aditya Jadhav <adityabjadhav8@gmail.com>
2025-07-12 14:00:00 +0530

    Update logicaloperators file

--- Day-2/logicalOperators.py
+++ Day-2/logicalOperators.py
@@ -5,13 +5,13 @@ a = True
 print(not a)     # False
 
 # and - returns the first falsy value it encounters, or the last value if all are truthy values .
-print(( True and 0 and "" and True) ) # 0
-print( ("" and 0 and True and True))  # ""
+print( True and 0 and "" and True ) # 0
+print( "" and 0 and True and True)  # ""
 
-print((True and True and 100))    # 100
+print(True and True and 100)    # 100
 
 # or - returns the first truthy value it finds, or the last falsy value if none are truthy values.
-print( (True or 0 or "" or True))  # True
-print( ("" or 0 or 100 or True))  # 100
+print( True or 0 or "" or True)  # True
+print( "" or 0 or 100 or True)  # 100
 
-print((False or 0 or ""))    # "" empty string
+print(False or 0 or "")    # "" empty string
