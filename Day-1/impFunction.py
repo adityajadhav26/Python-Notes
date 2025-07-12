cce014bdc639a66e8cfae5a9e91885c7d4c12758
Aditya Jadhav <adityabjadhav8@gmail.com>
2025-07-12 12:04:27 +0530

    Add imp functions file

---
+++ Day-1/impFunction.py
@@ -0,0 +1,21 @@
+
+name = "Aditya J"
+print(f"hello & welcome {name} !!")     # hello & welcome Aditya J !!
+print( type(name))  # <class 'str'>
+
+print(id(name))     # 4336981424
+print( isinstance(name, str))   # True
+
+
+num1 = None
+num2 = None
+
+num1 =  int(input ("enter  any number "))   # 25
+
+num2 =  int(input ("enter  any number "))   # 35
+
+sum = num1 + num2 
+
+print(f" addition of {num1} and {num2} is : {sum} ") # addition of 25 and 35 is : 60 
+
+print( isinstance(num1, int))   # True
